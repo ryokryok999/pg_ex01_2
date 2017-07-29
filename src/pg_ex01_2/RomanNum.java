23351f4e7b7b2a5873e4414b77fef7972c3c9317
z2020110 <z2020110@user-PC>
2017-07-29 11:29:56 +0900

    7/29修正

---
+++ src/pg_ex01_2/RomanNum.java
@@ -0,0 +1,66 @@
+/**
+ * RomnNum は、1以上20以下のアラビア数字を入力すると、
+ * ローマ数字に変換して表示するプログラムである。
+ */
+
+package pg_ex01_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RomanNum {
+
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in), 1);
+		try {
+			System.out.println("アラビア数字を入力して下さい。ただし、1以上20以下です。");
+			String line = reader.readLine();
+			int arabic_num = Integer.parseInt(line);
+			String roman_num = convertRomanNum(arabic_num);
+			System.out.println(roman_num);
+		} catch (IOException e) {
+			System.out.println(e);
+		} catch (NumberFormatException e) {
+			System.out.println("数字の形式が正しくありません。");
+		}
+
+	}
+	public static String convertRomanNum(int num) {
+		String roman_num="";
+		if ( num <= 0 || num >= 21 ) {
+			System.out.println("1以上20以下の数字を指定して下さい");
+		}else{
+			int num_x = num / 10;
+			int ichinokurai = num % 10;
+			int num_v = ichinokurai / 5;
+			int num_i = ichinokurai % 5;
+
+			roman_num = addCharacter(roman_num, "X", num_x);
+
+			if (ichinokurai == 4){
+				roman_num = addCharacter(roman_num, "IV", 1);
+			}else if(ichinokurai == 9){
+				roman_num = addCharacter(roman_num, "IX", 1);
+			}else{
+				roman_num = addCharacter(roman_num, "V", num_v);
+				roman_num = addCharacter(roman_num, "I", num_i);
+			}
+		}
+		return roman_num;
+	}
+	/**
+	 * addCharacter は、文字列に文字列を特定回数だけ追加します
+	 * @param str　元の文字列
+	 * @param s　追加する文字列
+	 * @param n　追加する回数
+	 * @return
+	 */
+	public static String addCharacter(String str, String s, int n) {
+		for (int i = 0; i < n; i++) {
+			str = str + s;
+		}
+		return str;
+	}
+
+}
